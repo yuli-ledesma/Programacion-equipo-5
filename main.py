88a8bc306825ba3ef68b48324f40fdfc67c1ae55
Victorio Paskevicius <pskvictorio@gmail.com>
2026-06-01 09:54:41 -0300

    Mejorando la legibilidad del menu

--- main.py
+++ main.py
@@ -1,18 +1,24 @@
 while True:
     # Mostrar menu
     print("Selecciona la opcion que deseas realizar")
-    print("1) Calcular comision")
-    print("2) Ver recibos de sueldo de vendedores (Admin)")
-    print("3) Salir")
+    print("1- Calcular comision")
+    print("2- Ver recibos de sueldo de vendedores (Admin)")
+    print("3- Salir")
     opc = int(input("Opcion: "))
 
     # Ralizar accion del menu
     match opc:
         case 1:
+            print("|------------------|")
             print("calcular comision")
+            print("|------------------|")
         case 2:
+            print("|------------------|")
             print("Ver recibos")
+            print("|------------------|")
         case 3:
+            print("|------------------|")
             print("Saliendo...")
+            print("|------------------|")
             exit()
 
