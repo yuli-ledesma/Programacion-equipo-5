7dff3e83f92f1051d387608cf5c616ab4ac828bb
kiaraa <kikivalfredo@gmail.com>
2026-06-03 17:50:07 -0600

    funcion mostrar liquidacion

---
+++ JAVASCRIPT/script.js
@@ -0,0 +1,21 @@
+let nombre = prompt('porfavor ingrese tu nombre: ')
+let apellido = prompt('Porfavor ingrese tu apellido: ')
+alert(`Bienvenid@ a nuestra página ${nombre} ${apellido} !`)
+
+
+let numero1 = Int(prompt('Ingrese el primer número: '))
+let numero2 = parseInt(prompt('Ingrese el segundo número: '))
+let resultado = numero1 + numero2
+alert(`El resultado de la suma es: ${resultado}`)
+
+
+let numero = Number(prompt('Por favor ingresa un numero triple: '));
+let numeroTriple = numero * 3
+console.log(`El triple de este número es: ${numeroTriple}`);
+
+let texto = prompt('Por favor ingresa un texto: ');
+let textoRepetido = texto + ' ' + texto
+console.log(`El texto repetido es: ${textoRepetido}`);
+
+
+
